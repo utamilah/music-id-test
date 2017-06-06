ac418f1c3e1939ac508f5a0eae331687e65c6447
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-06-05 21:58:26 -0400

    add router and login functions

--- authorization_code/public/js/app.js
+++ authorization_code/public/js/app.js
@@ -3,3 +3,26 @@ angular
   "ui.router",
   "ngResource"
 ])
+.config([
+  "$stateProvider",
+  Router
+])
+.controller("LogIn", [
+  "$scope",
+  "$http",
+  LogInControllerFunction
+])
+
+function Router($stateProvider) {
+  $stateProvider
+  .state("index", {
+    url: "/",
+    templateUrl: "js/ng-views/index.html",
+    controller: "IndexController",
+    controllerAs: "vm"
+  })
+}
+
+function LogInControllerFunction($scope, $http) {
+  
+}
