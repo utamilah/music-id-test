f952409672ebd2179c531b506cca14cb0d4a93fa
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-06-06 11:54:35 -0400

    set up user show page

--- authorization_code/public/js/app.js
+++ authorization_code/public/js/app.js
@@ -8,15 +8,20 @@ angular
   "$stateProvider",
   Router
 ])
+.factory("UserShowFactory", [
+  "$resource",
+  UserShowFactoryFunction
+])
 .controller("LogIn", [
   "$scope",
   "$http",
   LogInControllerFunction
 ])
-.controller("Callback", [
+.controller("UserShow", [
   "$scope",
   "$http",
-  CallbackControllerFunction
+  "UserShowFactory",
+  UserShowFunction
 ])
 
 function Router($locationProvider, $stateProvider) {
@@ -27,10 +32,10 @@ function Router($locationProvider, $stateProvider) {
     controller: "LogIn",
     controllerAs: "vm"
   })
-  .state("callback", {
-    url: "/callback",
+  .state("usershow", {
+    url: "/access_token=:access_token",
     templateUrl: "js/ng-views/main.html",
-    controller: "Callback",
+    controller: "UserShow",
     controllerAs: "vm"
   })
 }
@@ -39,58 +44,10 @@ function LogInControllerFunction($scope, $http) {
   console.log('log in here')
 }
 
-function CallbackControllerFunction($scope, $http) {
-  function getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
+function UserShowFactoryFunction($resource) {
+  return $resource("https://api.spotify.com/v1/me/top/artists/:id")
+}
 
-  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-      userProfileTemplate = Handlebars.compile(userProfileSource),
-      userProfilePlaceholder = document.getElementById('user-profile');
-
-  var oauthSource = document.getElementById('oauth-template').innerHTML,
-      oauthTemplate = Handlebars.compile(oauthSource),
-      oauthPlaceholder = document.getElementById('oauth');
-
-  var params = getHashParams();
-
-  var access_token = params.access_token,
-      refresh_token = params.refresh_token,
-      error = params.error;
-
-  if (error) {
-    alert('There was an error during the authentication');
-  } else {
-    if (access_token) {
-      // render oauth info
-      oauthPlaceholder.innerHTML = oauthTemplate({
-        access_token: access_token,
-        refresh_token: refresh_token
-      });
-
-      $.ajax({
-          url: 'https://api.spotify.com/v1/me',
-          headers: {
-            'Authorization': 'Bearer ' + access_token
-          },
-          success: function(response) {
-            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-
-            $('#login').hide();
-            $('#loggedin').show();
-          }
-      });
-    } else {
-        // render initial screen
-        $('#login').show();
-        $('#loggedin').hide();
-    }
-
-  }
+function UserShowFunction($scope, $http, UserShowFactory) {
+  console.log('logged in')
 }
