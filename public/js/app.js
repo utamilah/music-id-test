fb2e02487e5287feb5d4f3a9016c96546fca53c7
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-06-07 13:00:04 -0400

    text views adjust api request

--- public/js/app.js
+++ public/js/app.js
@@ -45,7 +45,7 @@ function Router($locationProvider, $stateProvider) {
 
 function User($location, $resource) {
   let url_tokens = $location.$$path
-  let access_token = url_tokens.substr(url_tokens.indexOf('=') +1)
+  let access_token = url_tokens.substr(url_tokens.indexOf('='))
 
   return $resource('https://api.spotify.com/v1/me', {}, {
       get: {
@@ -53,6 +53,8 @@ function User($location, $resource) {
           headers:{'Authorization':'Bearer ' + access_token}
       }
   });
+
+  console.log()
 }
 
 function LogInControllerFunction($scope, $http) {
@@ -61,7 +63,10 @@ function LogInControllerFunction($scope, $http) {
 
 function UserShowFunction($scope, $http, $stateParams, $location, User) {
   let url_tokens = $location.$$path
-  let access_token = url_tokens.substr(url_tokens.indexOf('=') +1)
+  let access_token = url_tokens.substr(url_tokens.indexOf('=') + 1)
+
+  let self = this
+  console.log(access_token)
 
   let urlUser = "https://api.spotify.com/v1/me"
   $http.get(urlUser, {headers:{'Authorization':'Bearer ' + access_token}})
@@ -77,8 +82,12 @@ function UserShowFunction($scope, $http, $stateParams, $location, User) {
   let urlArtist = "https://api.spotify.com/v1/me/top/artists"
   $http.get(urlArtist, {headers:{'Authorization':'Bearer ' + access_token}})
   .success( function(response){
-    this.artist = response
-    let artistInfo = artist.items[0].name
-    console.log(artistInfo)
+    self.artists = response
+  })
+
+  let urlTracks = "https://api.spotify.com/v1/me/top/tracks"
+  $http.get(urlTracks, {headers:{'Authorization':'Bearer ' + access_token}})
+  .success( function(response){
+    self.tracks = response
   })
 }
